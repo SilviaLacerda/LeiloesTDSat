2e75bea6246071d0c9d45a188d7b931c6b9170cd
SilviaLacerda <silvia.lacerdacan@gmail.com>
2023-08-26 15:03:05 -0300

    funcionalidade salvar

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -1,42 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
 /**
  *
- * @author Adm
+ * @author Silvia Lacerda
  */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 
-
 public class ProdutosDAO {
-    
+
     Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+    conectaDAO conexao;
+
+    public ProdutosDAO() {
+        this.conexao = new conectaDAO();
+        this.conn = this.conexao.connectDB();
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
+
+    public void cadastrarProduto(ProdutosDTO produto) {
+        // conn = new conectaDAO().connectDB();
+        String sql = "INSERT INTO produtos(nome, valor, status) VALUES "
+                + "(?, ?, ?)";
+        try {
+            PreparedStatement stmt = this.conn.prepareStatement(sql);
+            stmt.setString(1, produto.getNome());
+            stmt.setInt(2, produto.getValor());
+            stmt.setString(3, produto.getStatus());
+            stmt.execute();
+           
+            //tratando o erro, caso ele ocorra
+        } catch (Exception e) {
+            System.out.println("Erro ao inserir cadastro de produtos: " + e.getMessage());
+        }
+    }
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
+
         return listagem;
     }
-    
-    
-    
-        
-}
 
+}
